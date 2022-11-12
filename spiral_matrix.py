b1e2b6775b11c64c00c7adf5766d5f50f956d9b3
Menashe <menasheho@gmail.com>
2022-11-12 23:39:29 +0200

    spiral answer

--- spiral_matrix.py
+++ spiral_matrix.py
@@ -4,4 +4,19 @@ Example:
 Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
 Output: [1,2,3,6,9,8,7,4,5]
 '''
-print('f')
+def spiralOrder(matrix):
+	res = []
+	while matrix:
+		res += matrix.pop(0)
+		if matrix and matrix[0]:
+			for row in matrix:
+				res.append(row.pop())
+		if matrix:
+			res +=matrix.pop()[::-1]
+		if matrix and matrix[0]:
+			for row in matrix[::-1]:
+				res.append(row.pop(0))
+	return res
+mat1 = [[1,2,3],[4,5,6],[7,8,9]]
+mat2 = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+print(spiralOrder(mat2))
